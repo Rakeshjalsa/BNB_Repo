fdeff38968a044581ebd4416b78cba412fa6d801
Rakeshjalsa <Rakeshjalsa143@gmail.com>
2024-01-10 21:30:50 +0530

    all changed applied

--- greet.java
+++ greet.java
@@ -1,11 +1,11 @@
-<<<<<<< Updated upstream
-//tast 101
-int a=10
-int b=20
-=======
 //tast 100
 welcome variable{
     int x= y
     int y=z
 }
->>>>>>> Stashed changes
+
+//tast 101
+int a=10
+int b=20
+
+
