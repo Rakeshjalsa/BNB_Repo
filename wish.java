956507e34ba8a45c4da4a66bba0649b521eee748
Rakeshjalsa <Rakeshjalsa143@gmail.com>
2024-01-13 18:41:53 +0530

    hello class is modified

--- wish.java
+++ wish.java
@@ -1,7 +1,14 @@
 hi,hello
-=======
+
 hello class{
+<<<<<<< Updated upstream
     //tast 99
     variable x=2
     variable y=3
+=======
+    //tast 98
+    int m=2
+    int n=3
+    
+>>>>>>> Stashed changes
 }
